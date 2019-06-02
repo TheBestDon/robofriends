369879882d1d6bd1caf40eba7e8aa2f280b0913c
Donatas <donatas_bakanas@yahoo.com>
2019-06-02 11:58:45 +0300

    Updates

---
+++ precache-manifest.ef465ea61d192fdee0679ed6a5c4b2ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2f546069940e124dc6fffaea2aca4a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5d9d0bc612919b0c2d0e",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "832d9fed54c5f58a2fb7",
+    "url": "/robofriends/static/css/main.c5f8d014.chunk.css"
+  },
+  {
+    "revision": "5d9d0bc612919b0c2d0e",
+    "url": "/robofriends/static/js/2.b3345a38.chunk.js"
+  },
+  {
+    "revision": "832d9fed54c5f58a2fb7",
+    "url": "/robofriends/static/js/main.67b0552a.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "f5bb43bf13faa5ec998710d19cf11085",
+    "url": "/robofriends/static/media/SEGA.f5bb43bf.woff"
+  }
+]);
